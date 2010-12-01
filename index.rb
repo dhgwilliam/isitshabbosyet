799f852e15be18475d82eff4aeade30d161c252e
David Gwilliam <dhgwilliam@gmail.com>
2010-12-01 17:38:55 -0500

    first commit

---
+++ index.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'date'
+require 'icalendar'
+require 'net/https'
+
+
+get '/' do
+  is_it_shabbos_yet
+end
+
+helpers do
+  def is_it_shabbos_yet
+    @shabbos = get_shabbos(11211)
+    @is_it = "Nope"
+#    @today = Date.today
+    @today = @shabbos + 1
+    if @today > @shabbos
+      @is_it = "Yep"
+    end
+    haml :index    
+  end 
+
+  def get_shabbos(zipcode)
+    shabbos_ical_url = URI.parse('http://www.chabad.org/calendar/candlelighting/candlelighting.ics_cdo/z/' + zipcode.to_s + '/weeks/')
+    shabbos_ical = Net::HTTP.get(shabbos_ical_url)
+    shabbos_ical.slice!(/CHARSET:utf-8\r\n/)
+    shabbos_ical = Icalendar.parse(shabbos_ical)
+    shabbos = shabbos_ical.first.events.first.dtstart
+  end
+
+end
