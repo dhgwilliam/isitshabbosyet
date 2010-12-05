9a7142b244e029763ac452fcbd20fc030c5f6223
David Gwilliam <dhgwilliam@gmail.com>
2010-12-04 21:47:54 -0500

    added if, elsif conditions for friday and saturday to make messaging more distinct

--- index.rb
+++ index.rb
@@ -14,10 +14,18 @@ helpers do
     @shabbos_start = @shabbos_event.first.events.first.dtstart
     @shabbos_end = @shabbos_event.first.events.first.dtend
     @today = DateTime.now
-#    @is_it = "Nope shabbos: "+ @shabbos.to_s + " and now it's: " + @today.to_s
-    @is_it = "Nope. Shabbos ended at " + @shabbos_end.hour.modulo(12).to_s + ":" + @shabbos_end.min.to_s + " in Williamsburg, Brooklyn"
-    if @today > @shabbos_start && @today < @shabbos_end
-      @is_it = "Yep. Shabbos started at " + @shabbos_start.hour.modulo(12).to_s + ":" + @shabbos.min.to_s + " pm on Friday and ends at " + @shabbos_end.hour.modulo(12).to_s + ":" + @shabbos_end.min.to_s + " in Williamsburg, Brooklyn"
+    @location = "Williamsburg, Brooklyn"
+
+    if @today.cwday == 5 && @today > @shabbos_start
+      @is_it = "Yep. Shabbos started at " + @shabbos_start.hour.modulo(12).to_s + ":" + @shabbos_start.min.to_s + " pm on Friday and ends at " + @shabbos_end.hour.modulo(12).to_s + ":" + @shabbos_end.min.to_s + " tomorrow in " + @location
+    elsif @today.cwday == 5 && @today < @shabbos_start
+      @is_it = "Not yet. Shabbos starts at " + @shabbos_start.hour.modulo(12).to_s + ":" + @shabbos.min.to_s + " pm in " + @location
+    elsif @today.cwday == 6 && @today < @shabbos_end
+      @is_it = "Yep. Shabbos ends at " + @shabbos_end.hour.modulo(12).to_s + ":" + @shabbos_end.min.to_s + " pm in " + @location
+    elsif @today.cwday == 6 && @today > @shabbos_end
+      @is_it = "Nope. Shabbos ended at " + @shabbos_end.hour.modulo(12).to_s + ":" + @shabbos_end.min.to_s + " pm in " + @location
+    else
+      @is_it = "Nope."
     end
     haml :index    
   end 
@@ -27,7 +35,6 @@ helpers do
     shabbos_ical = Net::HTTP.get(shabbos_ical_url)
     shabbos_ical.slice!(/CHARSET:utf-8\r\n/)
     shabbos_ical = Icalendar.parse(shabbos_ical)
-#    shabbos = shabbos_ical.first.events.first.dtstart
   end
 
 end
